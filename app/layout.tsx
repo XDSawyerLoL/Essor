553aff3d3530e45953231b4a10b6bbd50503db08
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-08-15 21:02:22 +0200

    Charge les styles Growth V3

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Fredoka, IBM_Plex_Mono, IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import "./v2.css";
 import "./agenda.css";
+import "./growth-v3.css";
 
 const fredoka = Fredoka({ variable: "--font-display", subsets: ["latin"] });
 const plexSans = IBM_Plex_Sans({ variable: "--font-body", subsets: ["latin"], weight: ["400", "500", "600", "700"] });
