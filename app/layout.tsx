da568c96027bfdf57b99b9eec677440a03eca401
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-08-15 20:22:50 +0200

    Charge les styles de l’agenda ESSOR

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Fredoka, IBM_Plex_Mono, IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import "./v2.css";
+import "./agenda.css";
 
 const fredoka = Fredoka({ variable: "--font-display", subsets: ["latin"] });
 const plexSans = IBM_Plex_Sans({ variable: "--font-body", subsets: ["latin"], weight: ["400", "500", "600", "700"] });
